8c160dcccebec5a9564a4eb8cb46f8598e5d1305
Nikhil kumar <nikhilkumar14082005@gmail.com>
2025-12-19 16:28:28 +0530

    changes

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -8,8 +8,8 @@ export const sendToken = (user, res, statusCode = 200, message = "Success") => {
     // Set cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
